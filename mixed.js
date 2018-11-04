d5ff020f383c605d0d96d45dabc2aa741b977e20
Md. Sumon Islam <developer.sumon@gmail.com>
2018-11-04 23:09:17 +0600

    Create mixed.js

---
+++ mixed.js
@@ -0,0 +1,27 @@
+$("button[data-eid='hosting.myh.cpanel.account.header.cpanel_admin.click']")
+
+.logo img {
+    top: 150%;
+    position: absolute;
+    -webkit-transform: translate(0);
+    -ms-transform: translate(0);
+    transform: translate(0);
+    ms-transform: translate(0);
+    max-height: 110px;
+}
+
+.navbar-fixed-top .logo img {
+    top: 50%;
+    position: absolute;
+    -webkit-transform: translate(0, -50%);
+    -ms-transform: translate(0, -50%);
+    transform: translate(0, -50%);
+    ms-transform: translate(0, -50%);
+    max-height: 54px;
+}
+
+.master_header_sticky {
+    top: 0px;
+    -webkit-transform: translateY(0)!important;
+    transform: translateY(0)!important;
+}
